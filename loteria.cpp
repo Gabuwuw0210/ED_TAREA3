978ea163446c0bda23ccfe35dd2211c4c5cab03e
Gabu <97574076+Gabuwuw0210@users.noreply.github.com>
2023-11-24 14:53:28 -0300

    loteria.cpp

--- loteria.cpp
+++ loteria.cpp
@@ -1,16 +1,9 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include "hash.hpp"
 
-const int MAX_PREMIOS = 100;
-
-struct Premio {
-    int numero;
-    std::string descripcion;
-    bool entregado;
-};
-
-void cargarPremios(const char* archivo, Premio premios[], int& totalPremios) {
+void cargarPremios(const char* archivo, ranura premios[], int& totalPremios) {
     std::ifstream archivoPremios(archivo);
 
     if (archivoPremios.is_open()) {
@@ -27,7 +20,7 @@ void cargarPremios(const char* archivo, Premio premios[], int& totalPremios) {
     }
 }
 
-void realizarSorteo(Premio premios[], int totalPremios, int numerosComprados[], int k) {
+void realizarSorteo(ranura premios[], int totalPremios, int numerosComprados[], int k) {
     for (int i = 0; i < k; ++i) {
         bool premioEncontrado = false;
 
@@ -50,7 +43,7 @@ int main() {
     const char* archivoPremios = "premios.txt";
     const int MAX_PERSONAS = 100;
 
-    Premio premios[MAX_PREMIOS];
+    ranura premios[tamTabla];
     int totalPremios = 0;
 
     cargarPremios(archivoPremios, premios, totalPremios);
